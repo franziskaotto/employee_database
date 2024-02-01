d4ce437a439c1e3975b0405d1416ecbaf78c32b4
Franziska Otto <franziska.otto@gmx.at>
2024-02-01 12:10:45 +0100

    removed a file

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const EquipmentModel = require("./db/equipment.model");
 
 //routes
 const employeeRoute = require("./routes/employees.route");
-const equimpentRoute = require("./routes/equipment.route")
+const equipmentRoute = require("./routes/equipment.route")
 
 const { MONGO_URL, PORT = 8080 } = process.env;
 
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/employees/", employeeRoute);
-app.use("/api/equipment/", equimpentRoute);
+app.use("/api/equipment/", equipmentRoute);
 
 
 
